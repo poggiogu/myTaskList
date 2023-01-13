b136ccee6f4e62eefc5bd65a5a5afba619c56e42
poggiogu <poggiogu@gmail.com>
2023-01-12 21:35:01 -0300

    commit del mock task e interface Task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASK:Task[] = [
+    {
+        id: 1,
+        text: "Terminar nuestro primer modulo de Angular",
+        day: 'Agosto 15, a las 12:00',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Hacer las compras para la cena",
+        day: 'Septiembre 21, a las 15:00',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: "Investigar Bootstrap",
+        day: 'Agosto 13, a las 09:00',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: "Leer código limpio",
+        day: 'noviembre 1, a las 14:00',
+         reminder: true,
+    }
+]
